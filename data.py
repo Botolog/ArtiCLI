e6954f6fcaa54f8de4d06df1cd2fae90cbf92b21
Botolog <81357028+Botolog@users.noreply.github.com>
2025-07-24 00:10:38 +0300

    moment of shock

--- data.py
+++ data.py
@@ -6,19 +6,23 @@ class TicketManager:
     def __init__(self):
         self.tickets = []
 
+    def clear(self):
+        self.tickets.clear()
+
     def reload(self, data):
+        self.clear()
         for ticket_data in data["tickets"]:
             self.tickets.append(Ticket(ticket_data))
 
-    def display(self, filters):
+    def display(self, option, filters):
         table = PrettyTable()
         table.align = "l"
         table.field_names=["id", "subject", "description", "status", "created_at"]
         for ticket in self.tickets:
-            if ticket.matching(filters):
+            if ticket.matching(option, filters):
                 table.add_row(ticket.asRow())
         if len(table.rows) == 0:
-            print("No Data. please load data first"); return 
+            print("No Data."); return 
         print(table)
 
 def prepro(field):
@@ -27,7 +31,12 @@ def prepro(field):
     return field
 
 def convertTime(time_string:str) -> datetime:
-    pass #TODO: make function
+    try:
+        dt_object = parser.parse(time_string)
+        dt_object = dt_object.replace(tzinfo=None, fold=1)
+        return dt_object
+    except ValueError:
+        return None 
 
 class Ticket:
     def __init__(self, ticket_data:dict):
@@ -35,16 +44,34 @@ class Ticket:
         self.subject     = prepro(ticket_data.get("subject"))
         self.description = prepro(ticket_data.get("description"))
         self.status      = prepro(ticket_data.get("status"))
-        self.created_at  = prepro(ticket_data.get("created_at"))
+        self.created_at  = convertTime(prepro(ticket_data.get("created_at")))
 
-    def matching(self, filters) -> bool:
+    def matching(self, option, filters) -> bool:
         if (filters == None): return True
-        if (filters in self.id or filters in self.subject or filters in self.description):
-            return True
+        elif (option == "search"):
+            return (filters.lower() in self.id.lower() or 
+                filters.lower() in self.subject.lower() or 
+                filters.lower() in self.description.lower() or
+                filters.lower() in self.status.lower()    
+            )
+        
+        elif (option == "after"):
+            if (self.created_at == None): return False
+            return (parser.parse(filters) < self.created_at)
+        elif (option == "before"):
+            if (self.created_at == None): return False
+            return (parser.parse(filters) > self.created_at)
+        
+        elif (option == "status"):
+            return (filters.lower() in self.status)
+        
+        else:
+            raise RuntimeError(f"no filter mode named {option}")
+            
         return False
     
     def asRow(self):
         return [self.id, self.subject, self.description, self.status, self.created_at]
     
-    def __str__(self):
-        return f"| {self.id} | {self.status}  \t| {self.subject} |\t |"
+    # def __str__(self):
+    #     return f"| {self.id} | {self.status}  \t| {self.subject} |\t |"
--- main.py
+++ main.py
@@ -11,12 +11,13 @@ def updateData(filename:str):
     DATA = new_data
     MANAGER.reload(DATA)
 
-def displayTickets(filters = None):
-    MANAGER.display(filters)
+def displayTickets(option = "search", filters = None):
+    MANAGER.display(option, filters)
 
 COMMANDS = {
     "update": Command("update", updateData),
-    "show": Command("show", displayTickets)
+    "show": Command("show", displayTickets),
+    "clear": Command("clear", MANAGER.clear)
 }
 
 
@@ -26,21 +27,21 @@ while True:
         command_name =  command_string[0]
         command_args = command_string[1:]
         command = COMMANDS[command_name]
-        command(*command_args)
-    #     try:
-    #         command(*command_args)
-    #     except Exception as E:
-    #         print(f"Error while executing \"{command_name}\": {E}")
-    #         raise SyntaxError
+        # command(*command_args)
+        try:
+            command(*command_args)
+        except Exception as E:
+            print(f"Error while executing \"{command_name}\": {E}")
+            raise SyntaxError
 
-    # except SyntaxError:
-    #     pass
+    except SyntaxError:
+        pass
     
     except KeyboardInterrupt:
         print("quitting...")
         quit()
     except KeyError as E:
         print(f"Command not found: \"{E.args[0]}\"")
-    # except Exception as E:
-    #     print(E)
-    #     quit()
+    except Exception as E:
+        print(E)
+        quit()
